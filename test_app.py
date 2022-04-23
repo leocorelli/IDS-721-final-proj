6f729bb2de2f9314d89a2da37031926a2b6b6595
lcorelli <leonardcorelli@gmail.com>
2022-04-23 14:13:11 -0400

    added functional test

--- test_app.py
+++ test_app.py
@@ -10,5 +10,5 @@ def test_root():
     assert response.status_code == 200
 
 def test_predict_quality():
-    response = client.get("/winequality")
+    response = client.get("/winequality?free_sulfur_dioxide=3.3&fixed_acidity=3.3&alcohol=3.3&density=3.3&sulphates=3.3&residual_sugar=3.3&citric_acid=3.3&total_sulfur_dioxide=3.3&pH=3.3&chlorides=3.3&volatile_acidity=3.3")
     assert response.status_code == 200
